8222a5917c8a61dccdca731fb32bc59960f69a6a
andrea <andrea.zollini@proton.me>
2024-05-28 20:47:35 +0200

    weather app flutter

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/services/weather_service.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:lottie/lottie.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService = WeatherService('3c1823e701eb71b30da0d341ff71b1a3');
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sunny.json';
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rainy.json';
+      case 'thunderstorm':
+        return 'assets/thunder.json';
+      case 'sunny':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? 'Loading city'),
+            Lottie.asset(getWeatherAnimation(_weather?.condition)),
+            Text('${_weather?.temperature.round()}°C'),
+            Text(_weather?.condition ?? ''),
+          ],
+        ),
+      ),
+    );
+  }
+}
